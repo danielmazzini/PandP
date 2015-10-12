214f2a59129f32c48aac5999a1eb3e58364e9da0
Daniel Mazzini <daniel.mazzini@gmail.com>
2015-10-13 01:50:38 +0200

    first commit

---
+++ Principios/1_DRY/ContactList/src/ContactList/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactList.Models;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContactList.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var contactList = new List<ContactGridItemViewModel> {new ContactGridItemViewModel("Daniel", "Mazzini", "daniel.mazzini@gmail.com", "Palacios 773", "Buenos Aires", "Argentina")};
+
+            return View(contactList);
+        }
+
+        public IActionResult GetContactFromCsv()
+        {
+            var contactList = new List<ContactGridItemViewModel> { new ContactGridItemViewModel("Daniel", "Mazzini", "daniel.mazzini@csv.com", "Palacios 773", "Buenos Aires", "Argentina") };
+
+            return View("Index",contactList);
+        }
+
+        public IActionResult GetContactFromService()
+        {
+            var contactList = new List<ContactGridItemViewModel> { new ContactGridItemViewModel("Daniel", "Mazzini", "daniel.mazzini@Service.com", "Palacios 773", "Buenos Aires", "Argentina") };
+
+            return View("Index", contactList);
+        }
+
+
+
+    }
+}
---
+++ Principios/1_DRY/ContactList/src/ContactList/Models/ContactGridItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactList.Models
+{
+    public class ContactGridItemViewModel
+    {
+        public ContactGridItemViewModel(string firstName, string lastName, string email, string address, string stateName, string countryName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Address = address;
+            StateName = stateName;
+            CountryName = countryName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public string Email { get; }
+
+        public string Address { get; }
+        public string StateName { get; }
+        public string CountryName { get; }
+   
+    }
+}
